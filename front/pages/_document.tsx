d6cb78479e050c2b522446c3256063fc56d9f4cf
gnos28 <gnos28@gmail.com>
2023-01-28 13:36:35 +0100

    add favicon

--- front/pages/_document.tsx
+++ front/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="fr">
-      <Head />
+      <Head>
+        <meta name="description" content="Les cartes de titiphe - voyance - cartomancie" />
+        <link rel="icon" href="/favicon96.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
