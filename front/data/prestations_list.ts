35d5cae855a8feca989fcb0492b3a9fade80338f
gnos28 <gnos28@gmail.com>
2023-02-05 17:39:23 +0100

    new cards backgrounds

--- front/data/prestations_list.ts
+++ front/data/prestations_list.ts
@@ -3,7 +3,7 @@ export const prestations_list = [
     name: "Question simple",
     description: "Je répond à votre question",
     price: 5,
-    background: "/card1.webp",
+    background: "/cards/card07.webp",
     link: "question-simple",
     description_long:
       'Question simple avec datation\nPour une réponse précise à votre question, merci d\'apporter autant de précision que possible dans le champ "informations complémentaires" prévu à cet effet.\nPar exemple :\nSi cela concerne plusieurs personne, indiquer leurs noms et leurs dates de naissance / age\nSi vous recherchez du travail / amour : depuis combien de temps ?\n\nLa réponse à votre tirage peut se faire de 3 façons :\n- Par téléphone, je vous appellerai selon le créneau fixé (me fournir votre numéro de téléphone)\n- Par instagram (me fournir votre pseudo instagram)\n- Par whatsapp, je vous appellerai selon le créneau fixé (me fournir votre numéro de téléphone)',
@@ -13,7 +13,7 @@ export const prestations_list = [
     name: "Tirage Général",
     description: "Plusieurs questions",
     price: 10,
-    background: "/card2.webp",
+    background: "/cards/card05.webp",
     link: "tirage-general",
     description_long:
       "Je vous éclaire sur votre avenir de manière générale avec plusieur oracles ou tarôts sur les thèmes suivants :\n- Sentimental\n- Travail / Finance\n- Relationnel / Familial\n\nLa guidance peut se faire de 3 façons :\n- Par téléphone, je vous appellerai selon le créneau fixé (me fournir votre numéro de téléphone)\n- Par instagram (me fournir votre pseudo instagram)\n- Par whatsapp, je vous appellerai selon le créneau fixé (me fournir votre numéro de téléphone)",
@@ -23,7 +23,7 @@ export const prestations_list = [
     name: "Guidance Générale",
     description: "Eclairage multi-thèmes",
     price: 20,
-    background: "/card2.webp",
+    background: "/cards/card01.webp",
     link: "guidance-generale",
     description_long:
       "Je vous éclaire sur votre avenir de manière générale avec plusieur oracles ou tarôts sur les thèmes suivants :\n- Sentimental\n- Travail / Finance\n- Relationnel / Familial\n\nLa guidance peut se faire de 3 façons :\n- Par téléphone, je vous appellerai selon le créneau fixé (me fournir votre numéro de téléphone)\n- Par instagram (me fournir votre pseudo instagram)\n- Par whatsapp, je vous appellerai selon le créneau fixé (me fournir votre numéro de téléphone)",
@@ -33,7 +33,7 @@ export const prestations_list = [
     name: "Lacher prise",
     description: "Réussir à faire la part des choses",
     price: 20,
-    background: "/card3.webp",
+    background: "/cards/card10.webp",
     link: "lacher-prise",
     description_long: "Réussir à faire la part des choses",
     duration: 30,
@@ -42,7 +42,7 @@ export const prestations_list = [
     name: "Soins énérgétique",
     description: "Amélioration du bien-être",
     price: 20,
-    background: "/card4.webp",
+    background: "/cards/card08.webp",
     link: "soins-energetique",
     description_long:
       "Ce soin est un allié parfait si tu souhaites :\n- Equilibrer les chakras\n- Accélerer les dons spirituels\n- Equilibrer les énergies subtiles du corps\n- Elargir le champ d'énergie ou vibratoire\n- Guérir les déséquilibres\n- Soulager le stress et la douleur\n- Activer le corps de lumière\n- Stimuler le système immunitaire\n- Activer les systèmes énergétiques\n- Permettre un bien-être général",
@@ -52,7 +52,7 @@ export const prestations_list = [
     name: "Désenvoutement et Protection",
     description: "Se libérer du mal",
     price: 50,
-    background: "/card4.webp",
+    background: "/cards/card09.webp",
     link: "desenvoutement",
     description_long:
       "Tu es victime d'attaques de forces, entités ou énergies négatives ?\nTu enchaines les difficultés dans divers domaines ?\nIl est temps de te libérer.\nCe nettoyage puissant aura comme effet dans ta vie :\n- Augmentation du taux vibratoire\n- Augmentation de ta chance en général\n- Moins de blocages\n- Meilleur état psychique\n- Augmentation de ton système immunitaire\n- Moins d'anxiété, plus de confiance en soi",
@@ -62,7 +62,7 @@ export const prestations_list = [
     name: "Séance de Spiritisme",
     description: "Message aux défunts",
     price: 50,
-    background: "/card4.webp",
+    background: "/cards/card03.webp",
     link: "spiritisme",
     description_long:
       "Tu es victime d'attaques de forces, entités ou énergies négatives ?\nTu enchaines les difficultés dans divers domaines ?\nIl est temps de te libérer.\nCe nettoyage puissant aura comme effet dans ta vie :\n- Augmentation du taux vibratoire\n- Augmentation de ta chance en général\n- Moins de blocages\n- Meilleur état psychique\n- Augmentation de ton système immunitaire\n- Moins d'anxiété, plus de confiance en soi",
