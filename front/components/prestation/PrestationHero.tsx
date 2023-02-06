89b2a75d640fbfb1e0e06a9fb009e65bfd60053f
gnos28 <gnos28@gmail.com>
2023-02-06 07:46:24 +0100

    interactive title for prestationHero

--- front/components/prestation/PrestationHero.tsx
+++ front/components/prestation/PrestationHero.tsx
@@ -4,6 +4,8 @@ import Prestation from "../Prestation";
 import Card from "react-animated-3d-card";
 import genericStyles from "../../styles/Prestation_details.module.scss";
 import styles from "./PrestationHero.module.scss";
+import { prestations_list } from "../../data/prestations_list";
+import Link from "next/link";
 
 type PrestationHeroProps = {
   prestation: {
@@ -18,9 +20,37 @@ type PrestationHeroProps = {
 };
 
 const PrestationHero = ({ prestation }: PrestationHeroProps) => {
+  const currentIndex = prestations_list.findIndex(
+    (presta) => presta.name === prestation.name
+  );
+
+  const previousIndex = currentIndex - 1 < 0 ? undefined : currentIndex - 1;
+  const nextIndex =
+    currentIndex + 1 > prestations_list.length - 1
+      ? undefined
+      : currentIndex + 1;
+
   return (
     <>
-      <h2>{prestation.name}</h2>
+      <h2 className={styles.interactiveTitle}>
+        <div>
+          {previousIndex !== undefined && (
+            <Link href={`${prestations_list[previousIndex].link}`}>
+              {prestations_list[previousIndex].name}
+            </Link>
+          )}
+        </div>
+        <div>
+          <span>{prestation.name}</span>
+        </div>
+        <div>
+          {nextIndex !== undefined && (
+            <Link href={`${prestations_list[nextIndex].link}`}>
+              {prestations_list[nextIndex].name}
+            </Link>
+          )}
+        </div>
+      </h2>
       <div className={styles.prestationDescriptionContainer}>
         <div className={styles.articlesContainer}>
           <Card key={prestation.name}>
