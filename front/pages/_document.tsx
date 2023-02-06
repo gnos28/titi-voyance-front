04f95dc494a7cc4cae4b4f8bce2ae34be6323c02
gnos28 <gnos28@gmail.com>
2023-02-06 21:12:29 +0100

    SEO optimization

--- front/pages/_document.tsx
+++ front/pages/_document.tsx
@@ -9,6 +9,23 @@ export default function Document() {
           content="Les cartes de titiphe, voyance et cartomancie"
         />
         <link rel="icon" href="/favicon96.ico" />
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
       </Head>
       <body>
         <Main />
