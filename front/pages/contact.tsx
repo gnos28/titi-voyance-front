7b85a36385713f7b5d23fd03999ea6e2740c1330
gnos28 <gnos28@gmail.com>
2025-06-22 18:45:23 +0200

    remove live

--- front/pages/contact.tsx
+++ front/pages/contact.tsx
@@ -50,7 +50,7 @@ const Contact: NextPageWithLayout = () => (
             </div>
           </Link>
         </div>
-        <h3>En live du lundi au vendredi à partir de 22h</h3>
+        {/* <h3>En live du lundi au vendredi à partir de 22h</h3> */}
       </div>
     </main>
   </>
